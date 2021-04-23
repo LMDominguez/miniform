f70567492627afcfea157c1176e3af73552e160f
michael.dominguez <lmdominguez@protonmail.com>
2021-04-23 07:16:55 -0700

    added User model

--- formtest/forms.py
+++ formtest/forms.py
@@ -1,6 +1,6 @@
 # Inside your app's forms.py file
-from django import forms
-class RegisterForm(forms.Form):
+from django import forms 
+class RegistrationForm(forms.Form):
     first_name = forms.CharField(max_length=45)
     last_name = forms.CharField(max_length=45)
     email = forms.EmailField()
